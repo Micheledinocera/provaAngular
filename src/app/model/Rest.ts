35a726a8d4f94159b72a38997829b6be8c400cd9
Michele Di Nocera <you@example.com>
2018-05-09 08:51:46 +0200

    cambiato url per chiamate countries e languages

--- src/app/model/Rest.ts
+++ src/app/model/Rest.ts
@@ -1,11 +1,17 @@
 import { HttpHeaders } from '@angular/common/http';
 
 export class Rest {
+  static protocol = 'http://';
+  // static protocol = 'https://';
+
+  static host = 'accela.dev1.accelasearch.net/';
+  // static host = 'svc10.accelasearch.io/';
+
   // get
-  static URL_COUNTRIES= 'http://dev1.accelasearch.net/countries';
-  static URL_LANGUAGES= 'http://dev1.accelasearch.net/languages';
+  static URL_COUNTRIES= Rest.protocol + Rest.host + 'countries';
+  static URL_LANGUAGES= Rest.protocol + Rest.host + 'languages';
   // post
-  static URL_NEW_CUSTOMER= 'http://accela.dev1.accelasearch.net/customers/newcustomer';
+  static URL_NEW_CUSTOMER= Rest.protocol + Rest.host + 'customers/newcustomer';
   // static URL_NEW_CUSTOMER= 'http://dev1.accelasearch.net/customers/newcustomer';
 
   static httpOptions = {
